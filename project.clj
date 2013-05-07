6a94e93e5463c8700c507aa32cc544d445c968e0
Roland Sadowski <szabla@gmail.com>
2013-05-07 17:14:18 +0100

    project.clj update, make the project usable by other projects

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
 (defproject rosado.arrowlets "0.1.0"
-  :description "Arrowlets. Making callback soup maintainabl."
-  :source-paths ["src-clj"]
+  :description "Arrowlets. Making callback soup maintainable."
+  :source-paths ["src-cljs"]
   :test-paths ["test-clj"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hiccup "1.0.0"]
-                 [ring "1.1.6"]
-                 [compojure "1.1.3"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
+                                  [hiccup "1.0.0"]
+                                  [ring "1.1.6"]
+                                  [compojure "1.1.3"]]}}
   :plugins [[lein-cljsbuild "0.2.10"]]
   :cljsbuild {:builds [{:source-paths "src-cljs"
-                         :compiler {:output-to "resources/public/js/main.js"
-                                    :optimization :simple
-                                    :pretty-print true}}]}
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimization :simple
+                                   :pretty-print true}}]}
+  :jar-exclusions [#".*public/js.*"]
   :ring {:handler rosado.cljs-base/app
          :auto-reload? true}
   :repl-options {:init-ns rosado.cljs-base})
